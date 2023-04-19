9d2093da8f3a5bfa8bf5d78cbb88a78118319db5
naeda <ryuya.maeda@esm.co.jp>
2023-04-19 11:24:01 +0900

    record.controllerの作成をした

--- backend/src/record/record.controller.ts
+++ backend/src/record/record.controller.ts
@@ -1,24 +1,87 @@
-import { Controller, Get, Post, Body, Param } from '@nestjs/common';
-import { PrismaClient, Monster, Record } from '@prisma/client';
-import { Param } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Put,
+  Delete,
+  Body,
+  Param,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
+import { RecordService } from './record.service';
+import { MonsterService } from './monster.service';
+import { Record } from '@prisma/client';
+import { CreateRecordDto } from './dto/create-record.dto';
+import { UpdateRecordDto } from './dto/update-record.dto';
 
 @Controller('record')
 export class RecordController {
-  private prisma = new PrismaClient();
+  constructor(
+    private readonly recordService: RecordService,
+    private readonly monsterService: MonsterService,
+  ) {}
 
   @Get()
-  async getAllMonsters(): Promise<Monster[]> {
-    return this.prisma.monster.findMany();
+  async getAllRecords(): Promise<Record[]> {
+    return this.recordService.findAll();
   }
 
-  async getMonsterById(@Param('id') id: string): Promise<Monster> {
-    return this.prisma.monster.findUnique({
-      where: { id: parseInt(id, 10) },
-    });
+  @Get(':id')
+  async getRecordById(@Param('id') id: string): Promise<Record> {
+    const record = await this.recordService.findOne(parseInt(id, 10));
+    if (!record) {
+      throw new HttpException(
+        'Record with id ${id} not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return record;
   }
 
   @Post()
-  async create(@Body() data: any) {
-    return await this.prisma.record.create({ data });
+  async createRecord(
+    @Body() createRecordDto: CreateRecordDto,
+  ): Promise<Record> {
+    const monster = await this.monsterService.findOne(
+      createRecordDto.monster_id,
+    );
+    if (!monster) {
+      throw new HttpException(
+        'Monster with id ${createRecordDto.monster_id} not found.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    const record = await this.recordService.create(createRecordDto);
+    return record;
+  }
+
+  @Put(':id')
+  async updateRecord(
+    @Param('id') id: string,
+    @Body() updateRecordDto: UpdateRecordDto,
+  ): Promise<Record> {
+    const record = await this.recordService.update(
+      parseInt(id, 10),
+      updateRecordDto,
+    );
+    if (!record) {
+      throw new HttpException(
+        'REcord with id ${id} not found.',
+        HttpStatus.NOT_FOUND,
+      );
+    }
+    return record;
+  }
+
+  @Delete(':id')
+  async deleteRecord(@Param('id') id: string): Promise<void> {
+    const result = await this.recordService.delete(parseInt(id, 10));
+    if (result === 0) {
+      throw new HttpException(
+        'REcord with id ${id} not found',
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
 }
