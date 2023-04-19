f176cf870618ae878efe20d28e7b5a1e524e4881
naeda <ryuya.maeda@esm.co.jp>
2023-04-19 17:15:20 +0900

    swaggerを追加してRESTAPIの仕様を見れるようにした

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+  const config = new DocumentBuilder()
+    .setTitle('Time Attack App API')
+    .setDescription('Time Attack App API Description')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
